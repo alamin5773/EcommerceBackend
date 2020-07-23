8d140caf2b32ab341024fac330301490d73567ac
alamin5773 <alamin5773@gmail.com>
2020-07-23 13:01:42 +0600

    update

--- Context/DbInitializer.cs
+++ Context/DbInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,14 +11,69 @@ namespace Ecommerce
 {
     public static class DbInitializer
     {
-        public static void Initialize(DatabaseContext context)
+        public static void Initialize(DatabaseContext context, RoleManager<IdentityRole> roleManager, UserManager<IdentityUser> userManager) 
         {
             context.Database.EnsureCreated();
 
+
+            // Add code to insert role identity tables
+
+            if (!roleManager.RoleExistsAsync("admin").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "admin";
+                role.NormalizedName = "admin";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+
+            if (!roleManager.RoleExistsAsync("customer").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "customer";
+                role.NormalizedName = "customer";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+            // Add code to insert user identity tables
+
+            if (userManager.FindByNameAsync("admin").Result == null)
+            {
+                IdentityUser user = new IdentityUser();
+                user.UserName = "admin";
+                user.Email = "admin@ecommerceapp.com";
+
+
+                IdentityResult result = userManager.CreateAsync(user, "Admin@123").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user,"admin").Wait();
+
+                    Customer customer = new Customer();
+                    customer.EmailAddress = user.Email;
+                    customer.Name = user.UserName;
+                    customer.AspNetUserID = user.Id;
+                    customer.Password = user.PasswordHash;
+                    context.Add(customer);
+                    context.SaveChanges();
+
+
+
+                }
+            }
+
+
             
+
+
 
         }
 
        
+
+
     }
 }
